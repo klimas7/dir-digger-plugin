bc1de6c5d36edf5d538276d410e5907aeaade7ee
klimas7 <klimas7@gmail.com>
2017-09-24 19:56:42 +0200

    Parameter Definition Skeleton

---
+++ src/main/java/org/jenkinsci/plugins/dirdigger/DirDiggerDefinition.java
@@ -0,0 +1,37 @@
+package org.jenkinsci.plugins.dirdigger;
+
+import hudson.Extension;
+import hudson.model.ParameterDefinition;
+import hudson.model.ParameterValue;
+import hudson.model.StringParameterDefinition;
+import net.sf.json.JSONObject;
+import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+
+public class DirDiggerDefinition extends ParameterDefinition {
+
+    @DataBoundConstructor
+    public DirDiggerDefinition(String name, String description) {
+        super(name, description);
+    }
+
+    @Override
+    public ParameterValue createValue(StaplerRequest staplerRequest, JSONObject jsonObject) {
+        return null;
+    }
+
+    @Override
+    public ParameterValue createValue(StaplerRequest staplerRequest) {
+        return null;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends ParameterDescriptor
+    {
+        @Override
+        public String getDisplayName() {
+            return "Dir Digger";
+        }
+    }
+}
