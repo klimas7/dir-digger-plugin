b70b2972244acfce650b096df80c03d6b3707f51
klimas7 <klimas7@gmail.com>
2017-09-29 19:13:21 +0200

    Add json

--- src/main/resources/org/jenkinsci/plugins/dirdigger/javascript/dirDigger.js
+++ src/main/resources/org/jenkinsci/plugins/dirdigger/javascript/dirDigger.js
@@ -4,14 +4,56 @@ jQuery.noConflict();
 var DirDigger = DirDigger || (function($) {
         var instance = {};
 
-        function Digger(selections) {
+        function Digger(selections, name, url) {
             this.selections = selections;
+            this.name = name;
+            this.url = url;
+            this.initChangeHandler();
         }
 
         Digger.prototype.getSelections = function() {
             return this.selections;
         }
 
+        Digger.prototype.getName = function() {
+            return this.name;
+        }
+
+        Digger.prototype.getUrl = function() {
+                    return this.url;
+                }
+
+        Digger.prototype.initChangeHandler = function() {
+             var _self = this;
+
+             for (let lvl = 0; lvl < _self.getSelections().length - 1; lvl++) {
+                var selection = _self.getSelections()[lvl];
+
+                selection.change(function() {
+                    var values = [];
+                    for (let i=0; i <= lvl; i++) {
+                        values.push(_self.getSelections()[i].val())
+                    }
+
+                    let jsonValues = values.toJSON()
+                    jQuery.ajax({
+                        url: _self.getUrl()+'/fillValueItems?parameterName='+_self.getName()+'&jsonFileNames='+jsonValues,
+                        level: lvl
+                    }).done(function(listModelBox) {
+
+                        let selection = _self.getSelections()[this.level+1]
+                        selection.find('option').remove().end();
+                        jQuery.each(listModelBox.values, function(j, value) {
+                            selection.append(jQuery('<option>', {
+                                value: value.value,
+                                text: value.value
+                            }));
+                        });
+                        selection.trigger("change");;
+                    });
+                });
+             }
+        }
 
         instance.Digger = Digger;
         return instance;
